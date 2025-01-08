34ff9f13ffffa9161a8c40ca46de43bcd2dc07c9
Aman Fangeria <amanfangeria980@gmail.com>
2025-01-08 18:35:13 +0530

    fix: build issue

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,7 +1,11 @@
-import admin, { ServiceAccount } from "firebase-admin";
-const serviceAccount = require("../serviceAccount.json");
+import admin from "firebase-admin";
+import serviceAccount from "../serviceAccount.json";
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        privateKey: serviceAccount.private_key,
+        clientEmail: serviceAccount.client_email,
+        projectId: serviceAccount.project_id,
+    }),
 });
 
 const db = admin.firestore();
