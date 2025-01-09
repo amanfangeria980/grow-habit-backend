7809841d053a8f94d8d769ed26fe2a00cbca8555
Aman Fangeria <amanfangeria980@gmail.com>
2025-01-09 18:55:56 +0530

    chore: minor fix

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Replace escaped newlines with actual newlines
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined;
+
 admin.initializeApp({
     credential: admin.credential.cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
-            ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
-            : undefined,
+        privateKey: privateKey,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         projectId: process.env.FIREBASE_PROJECT_ID,
     }),
