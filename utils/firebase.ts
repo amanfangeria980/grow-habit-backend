de34a5b58e3ba7d81ac935346706f7c89479b399
Aman Fangeria <amanfangeria980@gmail.com>
2025-01-09 19:04:53 +0530

    chore: minor fix

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Replace escaped newlines with actual newlines
+
+// in local environment the private key should in double quotes while in production it should be without quotes
 const privateKey = process.env.FIREBASE_PRIVATE_KEY
     ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     : undefined;
