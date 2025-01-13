ffaf31748dbb7602774c21cf9d1a719d9f8ca047
Aman Fangeria <amanfangeria980@gmail.com>
2025-01-13 17:03:41 +0530

    fix: type error fix and script fix

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { createReflection } from "../controllers/user.controller";
 
 const userRouter = express.Router();
 
-userRouter.post("/reflect", createReflection);
+userRouter.post("/reflect", (req: Request, res: Response) => {
+    createReflection(req, res);
+});
 
 export default userRouter;
