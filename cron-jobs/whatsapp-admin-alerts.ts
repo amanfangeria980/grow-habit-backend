dc221fd538642e8a780f2b6786315a5c3e4f64f8
Aman Fangeria <amanfangeria980@gmail.com>
2025-01-14 12:08:35 +0530

    chore: testing completed

--- cron-jobs/whatsapp-admin-alerts.ts
+++ cron-jobs/whatsapp-admin-alerts.ts
@@ -2,8 +2,8 @@ import cron from "node-cron";
 import calculateTwoPointerStatusAdmin from "../utils/calculateTwoPointerStatusAdmin";
 
 export const adminTwoPointerStatusAlertMorning = cron.schedule(
-    // "0 10 * * *",
-    "* * * * *",
+    "0 10 * * *",
+    // "* * * * *",
     async () => {
         const phoneNumbers = process.env.PHONE_NUMBER;
         const day = new Date(Date.now()).getDate();
